d24b2ae8bed404927233b116a54fc2808b96a992
Nehaa7255 <nehamas04@gmail.com>
2023-04-10 23:21:29 +0530

    overloading

--- src/oops/Example_1.java
+++ src/oops/Example_1.java
@@ -1,4 +1,9 @@
-package oops;
-
-public class Example_1 {
-}
+class Example_1{
+static int add(int a, int b){return a+b;}
+static double add(double a, double b){return a+b;}
+        }
+class TestOverloading2{
+    public static void main(String[] args){
+        System.out.println(Example_1.add(11,11));
+        System.out.println(Example_1.add(12.3,12.6));
+    }}
