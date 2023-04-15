c10d80b8424bd20a950e8588e2db659011a8300b
Nehaa7255 <nehamas04@gmail.com>
2023-04-15 17:25:02 +0530

    Character input Question(Q.N:12)added

---
+++ src/Basic/twelve.java
@@ -0,0 +1,20 @@
+package Basic;
+import java.util.Scanner;
+
+public class twelve {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the gender in (M/F) : ");
+        String gender =sc.nextLine();
+        char genderChar= gender.charAt(0);
+        if(genderChar == 'M'){
+            System.out.println("Good Morning sir");
+        }
+        else if(genderChar == 'F'){
+            System.out.println("Good Morning Mam");
+        }
+        else{
+            System.out.println("Wrong input");
+        }
+    }
+}
