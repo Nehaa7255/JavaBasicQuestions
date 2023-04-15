8d254eba3b26b150a7a5e4477d2aeec190a41744
Nehaa7255 <nehamas04@gmail.com>
2023-04-15 17:29:19 +0530

    Character input Question(Q.N:13)added

---
+++ src/Basic/thirteen.java
@@ -0,0 +1,20 @@
+package Basic;
+import java.util.Scanner;
+
+public class thirteen {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the gender in (M/F) : ");
+        String gender =sc.nextLine();
+        char genderChar= gender.charAt(0);
+        if(genderChar == 'M'|| genderChar == 'm'){
+            System.out.println("Good Morning sir");
+        }
+        else if(genderChar == 'F' || genderChar == 'f' ){
+            System.out.println("Good Morning Mam");
+        }
+        else{
+            System.out.println("Wrong input");
+        }
+    }
+}
