68f14982fee3e014eb654e887bb0785a0c8b322a
Nehaa7255 <nehamas04@gmail.com>
2023-04-15 17:04:44 +0530

    AND and OR operator Question(Q.N:10)added

---
+++ src/Basic/ten.java
@@ -0,0 +1,23 @@
+package Basic;
+import java.util.Scanner;
+
+public class ten {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter 1st number : ");
+        int first_number =sc.nextInt();
+        System.out.print("Enter 2nd number : ");
+        int second_number=sc.nextInt();
+        System.out.print("Enter 3rd number : ");
+        int third_number=sc.nextInt();
+        if(first_number == second_number && second_number == third_number ){
+            System.out.println("All numbers are equal");}
+            else if(first_number==second_number || second_number==third_number || third_number==first_number){
+                System.out.println("Any of two numbers are equal");
+            }
+            else{
+                System.out.println("Numbers are not equal");
+            }
+    }
+    }
+
