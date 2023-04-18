0b50c6af41e47abb0d8f6d48592336893f2bab1d
Nehaa7255 <nehamas04@gmail.com>
2023-04-18 23:50:47 +0530

    factorial of a number(Q.N:34)added

---
+++ src/Basic/thirty_four.java
@@ -0,0 +1,17 @@
+//Factorial of a number
+
+package Basic;
+import java.util.Scanner;
+
+public class thirty_four {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int num;
+        int i,fact=1;
+        int number=5;//It is the number to calculate factorial
+        for(i=1;i<=number;++i){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}
