ed50ee6e5f1b0687c38c84ccb9b0b045b83eaf3b
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:35:56 +0530

    perfect number(Q.N:39)added

---
+++ src/Basic/thirty_nine.java
@@ -0,0 +1,13 @@
+/* Accept a number and check if it a perfect number or not.
+     A number whose sum of factors(excluding number itself)  = Number
+     Ex -  6 = 1, 2, 3 = 6
+ */
+
+package Basic;
+import java.util.Scanner;
+
+public class thirty_nine {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
