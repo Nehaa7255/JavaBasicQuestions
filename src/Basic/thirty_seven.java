d7873d9b3f0c699320cded3757fb106778a97044
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:31:41 +0530

    all factor(Q.N:37)added

---
+++ src/Basic/thirty_seven.java
@@ -0,0 +1,9 @@
+// Print all the factors of a number.
+package Basic;
+import java.util.Scanner;
+
+public class thirty_seven {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+    }
+}
