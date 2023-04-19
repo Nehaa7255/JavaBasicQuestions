c3d3d312769d6e57955916232957ff2e3cad7d75
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:33:35 +0530

    sum of all factor(Q.N:37)added

---
+++ src/Basic/thirty_eight.java
@@ -0,0 +1,9 @@
+//Print the sum of all factors of a number, 50 - 1 + 2 + 5 + 10 + 25 = 43
+package Basic;
+import java.util.Scanner;
+
+public class thirty_eight {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
