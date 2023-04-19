b5f34d2d8ecc90d9a21f60b6ac2dcdd754d21d4e
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:45:53 +0530

    prime no.(Q.N:42)added

---
+++ src/Basic/forty_two.java
@@ -0,0 +1,9 @@
+// Check if the number is Prime or not.
+package Basic;
+import java.util.Scanner;
+
+public class forty_two {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
