d29209639ddedd30cfc86c766bd1b6bb9a3bd283
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:29:53 +0530

    divisible in range(Q.N:36)added

---
+++ src/Basic/thirty_six.java
@@ -0,0 +1,9 @@
+// Print all the numbers which are either divisible by 3 or 5 in a range
+package Basic;
+import java.util.Scanner;
+
+public class thirty_six {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
