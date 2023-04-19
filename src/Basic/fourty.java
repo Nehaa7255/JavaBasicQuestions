44ae6f7277e97e775ad807b32ae97c4778ac2a5c
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:38:44 +0530

    Seperate each digit(Q.N:40)added

---
+++ src/Basic/fourty.java
@@ -0,0 +1,9 @@
+//Seperate each digit of a number and print it on the new line
+package Basic;
+import java.util.Scanner;
+
+public class fourty {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
