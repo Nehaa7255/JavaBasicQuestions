17d6db918ddc3b68f644e25a3d4223a57d80d7b6
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:45:53 +0530

    reverse no.(Q.N:43)added

---
+++ src/Basic/forty_three.java
@@ -0,0 +1,8 @@
+//Accept a number and print its reverse
+package Basic;
+import java.util.Scanner;
+public class forty_three {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
