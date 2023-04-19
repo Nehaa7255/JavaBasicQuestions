a7d0f7c7f61061831df3cf319b8548e0db6a6a2d
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:50:30 +0530

    check armstrong no.(Q.N:45)added

---
+++ src/Basic/forty_five.java
@@ -0,0 +1,12 @@
+/* Accept a number and check if it is a armstrong number (Sum of cube of all digits will be equal to origional number)
+       Ex - 407 = 64 + 0 + 343 = 407
+              153 = 1 + 125 + 27 = 153
+              */
+package Basic;
+import java.util.Scanner;
+
+public class forty_five {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+    }
+}
