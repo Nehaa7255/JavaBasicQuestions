ec66931185d38b51c17224f6ba790ca093a7e1c3
Nehaa7255 <nehamas04@gmail.com>
2023-04-19 23:27:55 +0530

    sum of even&odd in range(Q.N:35)added

---
+++ src/Basic/thity_five.java
@@ -0,0 +1,10 @@
+// Print the sum of all even & odd numbers in a range seperately.
+
+package Basic;
+import java.util.Scanner;
+public class thity_five {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+
+    }
+}
