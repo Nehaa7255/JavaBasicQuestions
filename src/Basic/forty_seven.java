330733fed41b01cd605a94dedccf7bc5206b7482
Nehaa7255 <nehamas04@gmail.com>
2023-04-20 23:45:22 +0530

    Student marks(Q.N:22)added

---
+++ src/Basic/forty_seven.java
@@ -0,0 +1,10 @@
+/*Accept a number and check if it is a strong number or not (Sum of factorial of each digit)
+       Ex- 145 = 1! + 4! + 5! = 145
+        */
+package Basic;
+import java.util.Scanner;
+public class forty_seven {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
