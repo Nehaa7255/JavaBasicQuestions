04a83c4357e40fbcb6b4e523619a82c224ee2cb4
Nehaa7255 <nehamas04@gmail.com>
2023-04-20 23:56:19 +0530

    Character input Question(Q.N:12)added

--- src/Basic/twelve.java
+++ src/Basic/twelve.java
@@ -1,3 +1,9 @@
+/*
+     Accept the gender from the user as char and print the respective greeting message
+     Ex - Good Morning Sir (on the basis of gender)
+
+ */
+
 package Basic;
 import java.util.Scanner;
 
