1e833228fcf3bc8db83a3418544c59887b991327
Nehaa7255 <nehamas04@gmail.com>
2023-04-23 23:40:30 +0530

    divisible by 3 or 5 in a range(Q.N:36)added

--- src/Basic/thirty_six.java
+++ src/Basic/thirty_six.java
@@ -1,9 +1,15 @@
-// Print all the numbers which are either divisible by 3 or 5 in a range
+// Print all the numbers which are either divisible by 3 or 5 in a rangepackage Basic;
 package Basic;
 import java.util.Scanner;
-
-public class thirty_six {
-    public static void main(String []args){
-        Scanner sc=new Scanner(System.in);
-    }
+ public class thirty_six {
+        public static void main(String []args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a Number :- ");
+            int n = sc.nextInt();
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
 }
