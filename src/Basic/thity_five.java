b85baf0b7543952595978310a801fa2aee8f1b23
Nehaa7255 <nehamas04@gmail.com>
2023-04-23 23:39:21 +0530

    sum of odd & even no. in a range(Q.N:35)added

--- src/Basic/thity_five.java
+++ src/Basic/thity_five.java
@@ -5,6 +5,25 @@ import java.util.Scanner;
 public class thity_five {
     public static void main(String []args){
         Scanner sc= new Scanner(System.in);
+        int n, sumE=0, sumO=0;
+        System.out.println("Enter the  number in array");
+        n= sc.nextInt();
+        int [] a=new int[n];
+        System.out.println("Enter the elements in array");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+for(int i=0;i<n;i++){
+    if(a[i]%2==0){
+        sumE=sumE +a[i];
+
+    }
+    else{
+        sumO= sumO +a[i];
+    }
+    System.out.println("Sum of Even Numbers:"+sumE);
+    System.out.println("Sum of Odd Numbers:"+sumO);
+}
 
     }
 }
