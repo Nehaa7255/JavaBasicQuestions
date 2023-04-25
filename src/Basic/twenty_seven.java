58ba50e3b0bfcb0c1c7c998b0f862ee322097db4
Nehaa7255 <nehamas04@gmail.com>
2023-04-25 23:30:47 +0530

    alphabet(Q.N:27)added

--- src/Basic/twenty_seven.java
+++ src/Basic/twenty_seven.java
@@ -1,6 +1,7 @@
 // Accept an english alphabet from user and check if it is a consonant or a vowel;
 
 package Basic;
+import java.util.*;
 import java.util.Scanner;
 public class twenty_seven {
     public static void main(String []args){
