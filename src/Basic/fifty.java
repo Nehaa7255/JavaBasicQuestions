7b34bf391bef5c839aa592fcc47feca39a8111be
Nehaa7255 <nehamas04@gmail.com>
2023-04-27 23:45:31 +0530

    Question 50 added

--- src/Basic/fifty.java
+++ src/Basic/fifty.java
@@ -1,9 +1,11 @@
 //Make a choice based calculator using do while
 package Basic;
 import java.util.*;
+import java.util.Scanner;
 
 public class fifty {
     public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
 
     }
 }
