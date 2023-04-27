8595fddce4f21f648b21fdcdb5688b2dcf8c51d1
Nehaa7255 <nehamas04@gmail.com>
2023-04-27 23:47:18 +0530

    check armstrong number

--- src/Basic/armstrong.java
+++ src/Basic/armstrong.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class armstrong {
     public static void main(String[]args){
-        int num, cubesum = 0, digit, originalnum;
-        System.out.printf("enter the number : ");
+        int n, cubesum = 0, digit, originalnum;
+        System.out.print("enter the number : ");
         Scanner sc=new Scanner(System.in);
-        num=sc.nextInt();
-        originalnum=num;
-        while(num!=0){
-            digit= num%10;
+        n=sc.nextInt();
+        originalnum=n;
+        while(n!=0){
+            digit= n%10;
             cubesum+= Math.pow(digit, 3);
-            num/=10;
+            cubesum+= Math.pow(digit, 3);
+            n/=10;
         }
         if(cubesum==originalnum){
             System.out.println(originalnum+" "+ "is armstrong number");
