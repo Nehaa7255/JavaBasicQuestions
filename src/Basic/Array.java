6c16def48dcfa676169fc3f7788bccd11fc49cac
Nehaa7255 <nehamas04@gmail.com>
2023-04-27 23:48:03 +0530

    array

--- src/Basic/Array.java
+++ src/Basic/Array.java
@@ -3,12 +3,13 @@ import java.lang.*;
 import java.util.*;
 public class Array {
     public static void main(String args[]){
-        int a[]=new int[5];
+        int a[]=new int[6];
         a[0]=10;//initialization
         a[1]=20;
         a[2]=70;
         a[3]=40;
         a[4]=50;
+        a[5]=60;
         for(int i=0;i<a.length;i++)
             System.out.println(a[i]);
     }
