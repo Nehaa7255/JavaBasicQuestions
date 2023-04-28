185ab886b42e029781524bbe3007e6475b97cbd2
Nehaa7255 <nehamas04@gmail.com>
2023-04-28 22:54:24 +0530

    Shop discount(Q.N:54)added

--- src/Basic/twenty_three.java
+++ src/Basic/twenty_three.java
@@ -7,6 +7,7 @@ import java.util.Scanner.*;
 public class twenty_three {
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
+        System.out.println( );
 
     }
 }
