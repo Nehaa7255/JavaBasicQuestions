c6c79d3df57111fd0536d8a781db2d503e3e9f34
Nehaa7255 <nehamas04@gmail.com>
2023-04-28 23:39:54 +0530

    sum up to n term(Q.NO:33)added

--- src/Basic/thirty_three.java
+++ src/Basic/thirty_three.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class thirty_three {
     public static void main(String []agrs){
         Scanner sc= new Scanner(System.in);
-        int sum=0,n;
+        int sum=0;
+        int n;
         System.out.print("n= ");
          n= sc.nextInt();
         for(int i=1;i<=n;i++){
