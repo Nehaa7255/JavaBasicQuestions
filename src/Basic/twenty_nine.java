dd3457eb84df188a2b9c5abee31644cb398a0afa
Nehaa7255 <nehamas04@gmail.com>
2023-04-29 23:29:32 +0530

    hello world n times(Q.no:29)added

--- src/Basic/twenty_nine.java
+++ src/Basic/twenty_nine.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 public class twenty_nine {
     public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
-        int n;
         System.out.print("Enter a number : ");
-        n=sc.nextInt();
+      int n=sc.nextInt();
         for (int i = 1; i <=n; i++) {
             System.out.println( "Hello! World");
         }
