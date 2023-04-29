f3f20ffcb8334d88dc8f2091ad53bd5b045ba973
Nehaa7255 <nehamas04@gmail.com>
2023-04-29 23:32:28 +0530

    Question 32 added

--- src/Basic/thirty_two.java
+++ src/Basic/thirty_two.java
@@ -10,9 +10,8 @@ import java.util.Scanner;
 public class thirty_two {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        int num;
         System.out.print("Enter a number : ");
-        num=sc.nextInt();
+        int num=sc.nextInt();
         for(int i=1; i<=10;i++){
             System.out.println(num+"*"+i+" "+"="+" "+num*i);
         }
