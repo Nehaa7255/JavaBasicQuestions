407ca164401819b8bd772fc27901bc5f7cc39024
Nehaa7255 <nehamas04@gmail.com>
2023-04-29 23:34:38 +0530

    Question 33 added

--- src/Basic/thirty_three.java
+++ src/Basic/thirty_three.java
@@ -6,7 +6,6 @@ public class thirty_three {
     public static void main(String []agrs){
         Scanner sc= new Scanner(System.in);
         int sum=0;
-
         System.out.print("n= ");
         int n= sc.nextInt();
         for(int i=1;i<=n;i++){
