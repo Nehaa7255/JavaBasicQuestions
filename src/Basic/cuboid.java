12f74f0cfc962773a89d5b23b867fa170d946da4
Nehaa7255 <nehamas04@gmail.com>
2023-04-30 23:25:38 +0530

    cuboid

--- src/Basic/cuboid.java
+++ src/Basic/cuboid.java
@@ -1,16 +1,17 @@
 import java.util.*;
 import java.lang.*;
+import java.util.Scanner;
 public class cuboid {
     public static void main(String[] args) {
 
         Scanner sc= new Scanner (System.in);
         int length,breadth, height;
         int  area,volume;
-        System.out.printf("Enter the value of length : ");
+        System.out.print("Enter the value of length : ");
         length=sc.nextInt();
-        System.out.printf("Enter the value of breadth : ");
+        System.out.print("Enter the value of breadth : ");
         breadth=sc.nextInt();
-        System.out.printf("Enter the value of height : ");
+        System.out.print("Enter the value of height : ");
         height=sc.nextInt();
 
         area=(2*((length*breadth)+(breadth*height)+(height*length)));
