daa18db238d72b84e461bf6ea890a06becfdb0e6
Nehaa7255 <nehamas04@gmail.com>
2023-05-02 23:21:30 +0530

    all factors(Q.no:37)added

--- src/Basic/thirty_seven.java
+++ src/Basic/thirty_seven.java
@@ -6,7 +6,13 @@ public class thirty_seven {
     public static void main(String []args){
         Scanner sc= new Scanner(System.in);
         int n;
-
+        System.out.print("Enter the value of number : ");
+        n=sc.nextInt();
+for(int i=1;i<=n;i++){
+    if(n%i==0){
+        System.out.println(i+ " ");
+    }
+}
 
 
     }
