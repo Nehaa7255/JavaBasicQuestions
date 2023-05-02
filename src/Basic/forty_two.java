17e0a6b845206da1b374787986589dc540857a01
Nehaa7255 <nehamas04@gmail.com>
2023-05-02 23:19:27 +0530

    prime or not(Q.no:41)added

--- src/Basic/forty_two.java
+++ src/Basic/forty_two.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class forty_two {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number");
+        int num=sc.nextInt();
+        for(int i=2;i<=num;i++){
+            System.out.print("Enter a number");
+
+
+
+        }
+
     }
 }
