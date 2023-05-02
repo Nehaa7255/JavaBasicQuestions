e2f773812afdb4a6337326500495a75f891ae29a
Nehaa7255 <nehamas04@gmail.com>
2023-05-02 23:19:05 +0530

    sum of digit(Q.no:41)added

--- src/Basic/forty_one.java
+++ src/Basic/forty_one.java
@@ -1,5 +1,19 @@
 //Sum of digits of a number, 936 = 18
 package Basic;
+import java.util.*;
+import java.util.Scanner;
 
 public class forty_one {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+       int digitSum=0;
+        System.out.print("Enter a digit: ");
+        int num=sc.nextInt();
+       while(num>0){
+           int digit=num%10;
+           digitSum += digit;
+           num = num/10;
+       }
+        System.out.print("Sum of digit is: "+ digitSum);
+    }
 }
