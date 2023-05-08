16a76e5a409ed8022c98a50feabae98cd36865a0
Nehaa7255 <nehamas04@gmail.com>
2023-05-08 23:00:14 +0530

    (Q.no:44)added

--- src/Basic/forty_four.java
+++ src/Basic/forty_four.java
@@ -1,7 +1,6 @@
 /*Accept a number and check if it is a pallindromic number (If number and its reverse are equal)
        Ex - 12321 - Rerverse - 12321
        */
-
 package Basic;
 import java.util.*;
 import java.util.Scanner;
